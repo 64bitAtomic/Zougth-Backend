e17f899d2ef7ff212ae3c94d7169412fad39ca95
64bitAtomic <zk286187@gmail.com>
2026-05-20 14:14:36 +0530

    fix cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ['http://localhost:5173','https://zought.netlify.app'], credentials: true }));
+app.use(cors({ origin: [ FRONTEND,'https://zought.netlify.app'], credentials: true }));
 app.use(cookieParser());
 
 // Routes
